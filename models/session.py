157bbb668b60ba6f07001ebc399613a44820e4cd
Paulo Longino <poul@poulsociety.com>
2018-02-14 20:34:16 +0300

    Computed Fields

--- models/session.py
+++ models/session.py
@@ -1,15 +1,33 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api, exceptions, _
 
+
 class Session(models.Model):
+
+    """ Session Model """
     _name = 'openacademy.session'
 
-    name = fields.Char(required=True)
-    start_date = fields.Date(default=lambda self : fields.Date.today())
-    duration = fields.Float(digits=(6, 2), help="Duration in days", default=1)
+    name = fields.Char(required = True)
+    start_date = fields.Date(default = fields.Date.today())
+    duration = fields.Float(digits = (6, 2), help = "Duration in days", default = 1)
     seats = fields.Integer(string = "Number of seats")
-    instructor_id = fields.Many2one('res.partner', string="Instructor",
+    instructor_id = fields.Many2one('res.partner', string = "Instructor",
                                     domain=['|', ('instructor', '=', True),
                                             ('category_id.name', 'ilike', 'Teacher')])
-    course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
-    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    course_id = fields.Many2one('openacademy.course', ondelete = 'cascade', string = "Course", required = True)
+    attendee_ids = fields.Many2many('res.partner', string = "Attendees")
+    taken_seats = fields.Float(string = "Taken Seats", compute = "_taken_seats")
+    active = fields.Boolean(default = True)
+
+
+    @api.depends("seats", "attendee_ids")
+    def _taken_seats(self):
+
+        """ Compute Taken Seats in Percentages """
+
+        for r in self:
+            if not r.seats:
+                r.taken_seats = 0.0
+            else:
+                r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
+
