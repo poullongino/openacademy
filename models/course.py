79908c2d4344475a337fc265c5b9be9bce704f2b
Paulo Longino <poul@poulsociety.com>
2018-02-15 11:09:21 +0300

    Adding a duplicate option

--- models/course.py
+++ models/course.py
@@ -9,4 +9,34 @@ class Course(models.Model):
     description = fields.Text()
     responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
     session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
+    
+    @api.multi
+    def copy(self, default = None):
+
+        """ Copy Method which allow to duplicate the course Title """
+
+        default = dict(default or {})
+
+        copied_count =  self.search_count(
+            [('name', '=like', u"Copy of {}%".format(self.name))]
+        )
+        if not copied_count:
+            new_name = u"Copy of {}".format(self.name)
+        else:
+            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+
+        default['name'] = new_name
+        return super(Course, self).copy(default)
+            
+    
+    
+    _sql_constraints = [
+        ('name_description_check',
+         'CHECK(name != description)',
+         'The title of the course should not be the description'),
+        
+        ('name_unique',
+         'UNIQUE(name)',
+         "The course title must be unique"),
+    ]
 
--- models/session.py
+++ models/session.py
@@ -29,6 +29,24 @@ class Session(models.Model):
             'message': message,
         }}
 
+    @api.multi
+    def copy(self, default=None):
+
+        """ Copy Method which allow to duplicate The Session Title """
+
+        default = dict(default or {})
+
+        copied_count = self.search_count(
+            [('name', '=like', u"Copy of {}%".format(self.name))]
+        )
+        if not copied_count:
+            new_name = u"Copy of {}".format(self.name)
+        else:
+            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+
+        default['name'] = new_name
+        return super(Session, self).copy(default)
+
 
     @api.depends("seats", "attendee_ids")
     def _taken_seats(self):
@@ -67,11 +85,7 @@ class Session(models.Model):
 
 
     _sql_constraints = [
-        ('name_description_check',
-         'CHECK(name != description)',
-         "The title of the course should not be the description"),
-
         ('name_unique',
          'UNIQUE(name)',
-         "The course title must be unique"),
+         "The session title must be unique"),
     ]
